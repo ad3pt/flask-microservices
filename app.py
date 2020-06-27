1c671720048d04e8d5370719e42fc2567794f855
Sanjeev Dawalbhakta <sanjeev@Sanjeevs-MBP.hsd1.ca.comcast.net>
2020-06-27 10:34:56 -0700

    New file app.py for flask ms

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def index():
+    response_data={}
+    response_data['message']="welcome"
+    response_data['status']="success"
+    response=jsonify(response_data)
+    return response
+
